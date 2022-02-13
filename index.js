77d1e79ceda93775bd23b1a1c4ae00d199ac9cad
Joshua Hendrawan <joshuahendrawan03@gmail.com>
2022-02-13 19:44:33 +0700

    Adding Global Function, and table components

---
+++ index.js
@@ -0,0 +1,77 @@
+// Variable
+
+var globalIncomeData = [
+  {
+    "date": "1Jan2022",
+    "description": "Payment Refund From Gojek",
+    "source": "Gojek",
+    "amount": "500000"
+  },
+  {
+    "date": "2Jan2022",
+    "description": "Transferan Hutang dari Teman",
+    "source": "Bank",
+    "amount": "300000"
+  },
+  {
+    "date": "4Jan2022",
+    "description": "Hasil Jualan Toko",
+    "source": "Gojek",
+    "amount": "500000"
+  },
+  {
+    "date": "6Jan2022",
+    "description": "Gaji Masuk",
+    "source": "Bank",
+    "amount": "3000000"
+  },
+  {
+    "date": "10Jan2022",
+    "description": "Refund Pulsa",
+    "source": "OVO",
+    "amount": "200000"
+  },
+  {
+    "date": "22Jan2022",
+    "description": "Payment Refund From Gojek",
+    "source": "Gojek",
+    "amount": "100000"
+  }
+]
+
+var globalExpensesData = [
+  {
+    "date": "21Jan2022",
+    "description": "Bayar Tagihan Listrik",
+    "source": "Gojek",
+    "amount": "250000"
+  },
+  {
+    "date": "2Feb2022",
+    "description": "Top up OVO",
+    "source": "M-Banking",
+    "amount": "300000"
+  },
+  {
+    "date": "5Feb2022",
+    "description": "Top up GOJEK",
+    "source": "Gojek",
+    "amount": "300000"
+  },
+  {
+    "date": "10Feb2022",
+    "description": "Beli Pulsa",
+    "source": "OVO",
+    "amount": "100000"
+  },
+]
+
+var globalUserInfo = {
+  "name": "Joshua Hendrawan",
+  "balance": 5000000,
+  "dob": "03-03-1998"
+}
+
+
+// Function
+
