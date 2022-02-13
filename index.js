8f9cecd599e3ae20d2c5410bfc05208718f398d8
Joshua Hendrawan <joshuahendrawan03@gmail.com>
2022-02-13 22:37:15 +0700

    Add local storage for saving value, and incomes and expenses page

--- index.js
+++ index.js
@@ -1,77 +1,97 @@
 // Variable
 
-var globalIncomeData = [
-  {
-    "date": "1Jan2022",
-    "description": "Payment Refund From Gojek",
-    "source": "Gojek",
-    "amount": "500000"
-  },
-  {
-    "date": "2Jan2022",
-    "description": "Transferan Hutang dari Teman",
-    "source": "Bank",
-    "amount": "300000"
-  },
-  {
-    "date": "4Jan2022",
-    "description": "Hasil Jualan Toko",
-    "source": "Gojek",
-    "amount": "500000"
-  },
-  {
-    "date": "6Jan2022",
-    "description": "Gaji Masuk",
-    "source": "Bank",
-    "amount": "3000000"
-  },
-  {
-    "date": "10Jan2022",
-    "description": "Refund Pulsa",
-    "source": "OVO",
-    "amount": "200000"
-  },
-  {
-    "date": "22Jan2022",
-    "description": "Payment Refund From Gojek",
-    "source": "Gojek",
-    "amount": "100000"
-  }
-]
+var globalIncomeData = []
 
-var globalExpensesData = [
-  {
-    "date": "21Jan2022",
-    "description": "Bayar Tagihan Listrik",
-    "source": "Gojek",
-    "amount": "250000"
-  },
-  {
-    "date": "2Feb2022",
-    "description": "Top up OVO",
-    "source": "M-Banking",
-    "amount": "300000"
-  },
-  {
-    "date": "5Feb2022",
-    "description": "Top up GOJEK",
-    "source": "Gojek",
-    "amount": "300000"
-  },
-  {
-    "date": "10Feb2022",
-    "description": "Beli Pulsa",
-    "source": "OVO",
-    "amount": "100000"
-  },
-]
+var globalExpensesData = []
 
 var globalUserInfo = {
-  "name": "Joshua Hendrawan",
-  "balance": 5000000,
+  "name": "PapaBear",
+  "balance": 12500000,
   "dob": "03-03-1998"
 }
 
+let globalRoute = "tes"
 
 // Function
+
+function setGlobalIncomeData() {
+  globalIncomeData = [
+    {
+      "date": "1Jan2022",
+      "description": "Payment Refund From Gojek",
+      "source": "Gojek",
+      "amount": "500000"
+    },
+    {
+      "date": "2Jan2022",
+      "description": "Transferan Hutang dari Teman",
+      "source": "Bank",
+      "amount": "300000"
+    },
+    {
+      "date": "4Jan2022",
+      "description": "Hasil Jualan Toko",
+      "source": "Gojek",
+      "amount": "500000"
+    },
+    {
+      "date": "6Jan2022",
+      "description": "Gaji Masuk",
+      "source": "Bank",
+      "amount": "3000000"
+    },
+    {
+      "date": "10Jan2022",
+      "description": "Refund Pulsa",
+      "source": "OVO",
+      "amount": "200000"
+    },
+    {
+      "date": "22Jan2022",
+      "description": "Payment Refund From Gojek",
+      "source": "Gojek",
+      "amount": "100000"
+    }
+  ]
+  localStorage.setItem("globalIncomeData", JSON.stringify(globalIncomeData))
+}
+
+function setGlobalExpensesData() {
+  globalExpensesData = [
+    {
+      "date": "21Jan2022",
+      "description": "Bayar Tagihan Listrik",
+      "source": "Gojek",
+      "amount": "250000"
+    },
+    {
+      "date": "2Feb2022",
+      "description": "Top up OVO",
+      "source": "M-Banking",
+      "amount": "300000"
+    },
+    {
+      "date": "5Feb2022",
+      "description": "Top up GOJEK",
+      "source": "Gojek",
+      "amount": "300000"
+    },
+    {
+      "date": "10Feb2022",
+      "description": "Beli Pulsa",
+      "source": "OVO",
+      "amount": "100000"
+    },
+  ]
+  localStorage.setItem("globalExpensesData", JSON.stringify(globalExpensesData))
+}
+
+function setGlobalRoute() {
+  localStorage.setItem("globalRoute", globalRoute)
+}
+
+function resetRoute() {
+  route = ""
+  setGlobalRoute()
+}
 
